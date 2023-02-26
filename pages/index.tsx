c99a506f2b14b3a969a77b13fbdf724698257c85
theoccultcorner <35445349+theoccultcorner@users.noreply.github.com>
2023-02-25 19:09:12 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Generator</title>
+        <title>Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
